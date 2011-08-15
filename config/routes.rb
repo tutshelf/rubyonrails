a3a24310c3b105a9a0bd8dc050480710fb0b2b4a
tutshelf <tutshelf@gmail.com>
2011-08-15 15:47:39 +0530

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Tutshelf::Application.routes.draw do
+  get "users/new"
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
   match '/about', :to => 'pages#about'
- match '/', :to => 'pages#home'
+  match '/signup',  :to => 'users#new'
+ root :to => 'pages#home'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
