76b7d93310b3c9c1a392c51485908c0def689d20
tutshelf <tutshelf@gmail.com>
2011-08-15 11:13:04 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tutshelf::Application.config.secret_token = '5b3b8df2e848eeea566fbaad315365a0ab2a58eaf8516c00a7230cb4153378d2c1c9609067805052f4e4c9799174a752fd9ed8280ecc5622e213d2dd7e4288fc'
